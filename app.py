0a24ddf6e8a2d82cee175ecf082c531f7e0e02cd
shubhodbirajdar <shubhodbirajdar928@gmail.com>
2026-08-07 23:25:23 +0530

    Add and activate Privacy, Terms of Service, and Support Help Center pages

--- app.py
+++ app.py
@@ -559,6 +559,21 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
+
+@app.route("/support")
+def support():
+    return render_template("support.html")
+
+
 # =====================================================
 # AI Hub
 # =====================================================
